7355fcbdb82159a6772cded4a87a484686a7063c
Marco Tröster <marco@troester-gmbh.de>
2021-09-25 15:08:17 +0200

    refactored tests

--- tests/move_gen_test.cpp
+++ tests/move_gen_test.cpp
@@ -4,54 +4,68 @@
 #include "game.h"
 #include <chrono>
 
-game_info ginfo{ true,true,true };
+game_info ginfo{ true, true, true };
+
+namespace
+{
+	void assert_perft_result(perft_results res, perft_results exp)
+	{
+		REQUIRE(res.nodes == exp.nodes);
+		REQUIRE(res.captures == exp.captures);
+		REQUIRE(res.en_passants == exp.en_passants);
+		REQUIRE(res.castles == exp.castles);
+		REQUIRE(res.promos == exp.promos);
+	}
+
+	void validate_position(game g, int depth, perft_results exp)
+	{
+		auto res = g.perft(depth);
+		assert_perft_result(res, exp);
+	}
+
+	void benchmark_perft(int depth)
+	{
+		game g;
+		printf("========== BENCHMARK ==========\n");
+		printf("Initial Perft depth = %i\n", depth);
+		auto now = std::chrono::high_resolution_clock::now();
+		auto res = g.perft(depth);
+		auto then = std::chrono::high_resolution_clock::now();
+		printf("Print so that it doesn't optimize the result away: %i\n", (int)res.nodes);
+		printf("measured time in seconds: %f\n", std::chrono::duration_cast<std::chrono::milliseconds>(then - now).count() / 1000.f);
+		printf("===============================\n\n");
+	}
+}
 
 TEST_CASE("INIT")
 {
 	board::init_all();
 }
 
-
-// Position from chessprogramming.org
 TEST_CASE("Move Generation Perft Position 2")
 {
+	// Position from chessprogramming.org
 	std::string fen = "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - ";
 	game g(fen);
 
 	SECTION("depth = 1")
 	{
-		auto res = g.perft(1);
-		REQUIRE(res.castles == 2);
-		REQUIRE(res.captures == 8);
-		REQUIRE(res.nodes == 48);
+		validate_position(g, 1, perft_results{ 48, 8, 0, 2, 0 });
 	}
-	
+
 	SECTION("depth = 2")
 	{
-		auto res = g.perft(2);
-		REQUIRE(res.en_passants == 1);
-		REQUIRE(res.captures == 351);
-		REQUIRE(res.castles == 91);
-		REQUIRE(res.nodes == 2039);
+		validate_position(g, 2, perft_results{ 2039, 351, 1, 91, 0 });
 	}
 
 	SECTION("depth = 3")
 	{
-		auto res = g.perft(3);
-		REQUIRE(res.captures == 17102);
-		REQUIRE(res.nodes == 97862);
-		REQUIRE(res.en_passants == 45);
-		REQUIRE(res.castles == 3162);
+		validate_position(g, 3, perft_results{ 97862, 17102, 45, 3162, 0 });
 	}
 
 	SECTION("depth = 4")
 	{
-		auto res = g.perft(4);
-		REQUIRE(res.captures == 757163);
-		REQUIRE(res.nodes == 4085603);
-		REQUIRE(res.en_passants == 1929);
-		REQUIRE(res.castles == 128013);
-		REQUIRE(res.promos == 15172);
+		validate_position(g, 4, perft_results{ 4085603, 757163, 1929, 128013, 15172 });
 	}
 }
 
@@ -59,6 +73,7 @@ TEST_CASE("Move generation king")
 {
 	SECTION("King 1")
 	{
+		// TODO: transform position into FEN string
 		game g(
 			"..k....."
 			"........"
@@ -69,14 +84,13 @@ TEST_CASE("Move generation king")
 			"........"
 			"........", ginfo, ginfo, color::white, 0ULL
 		);
-		auto res = g.perft(1);
-		REQUIRE(res.nodes == 8);
+		validate_position(g, 1, perft_results{ 8, 0, 0, 0, 0 });
 	}
 
 	SECTION("King 2")
 	{
-
-		game g2(
+		// TODO: transform position into FEN string
+		game g(
 			"..k....."
 			"........"
 			"........"
@@ -86,9 +100,7 @@ TEST_CASE("Move generation king")
 			"....p..."
 			".....K..", ginfo, ginfo, color::white, 0ULL
 		);
-		auto res = g2.perft(1);
-		REQUIRE(res.captures == 1);
-		REQUIRE(res.nodes == 5);
+		validate_position(g, 1, perft_results{ 5, 1, 0, 0, 0 });
 	}
 }
 
@@ -96,6 +108,7 @@ TEST_CASE("Castling")
 {
 	SECTION("Queenside")
 	{
+		// TODO: transform position into FEN string
 		game_info ginfo_castling;
 		ginfo_castling.has_moved_kingside_rook = true;
 		ginfo_castling.has_moved_king = false;
@@ -109,12 +122,12 @@ TEST_CASE("Castling")
 			"........"
 			"........"
 			"....K...", ginfo, ginfo_castling, color::black, 0ull);
-		auto res = g.perft(1);
-		REQUIRE(res.castles == 1);
+		validate_position(g, 1, perft_results{ 15, 0, 0, 1, 0 });
 	}
 
 	SECTION("Kingside")
 	{
+		// TODO: transform position into FEN string
 		game_info ginfo_castling;
 		ginfo_castling.has_moved_queenside_rook = true;
 		ginfo_castling.has_moved_king = false;
@@ -128,8 +141,7 @@ TEST_CASE("Castling")
 			"........"
 			"........"
 			"....K...", ginfo, ginfo_castling, color::black, 0ull);
-		auto res = g.perft(1);
-		REQUIRE(res.castles == 1);
+		validate_position(g, 1, perft_results{ 14, 0, 0, 1, 0 });
 	}
 }
 
@@ -137,6 +149,7 @@ TEST_CASE("Move Generation pawn")
 {
 	SECTION("Pawn 1")
 	{
+		// TODO: transform position into FEN string
 		game g(
 			"..k....."
 			"........"
@@ -147,13 +160,12 @@ TEST_CASE("Move Generation pawn")
 			"........"
 			"K.......", ginfo, ginfo, color::white, 0ULL
 		);
-		auto res = g.perft(1);
-		REQUIRE(res.captures == 2);
-		REQUIRE(res.nodes == 6);
+		validate_position(g, 1, perft_results{ 6, 2, 0, 0, 0 });
 	}
 
 	SECTION("Pawn 2")
 	{
+		// TODO: transform position into FEN string
 		game g(
 			"k......."
 			"........"
@@ -164,13 +176,12 @@ TEST_CASE("Move Generation pawn")
 			"PPP....."
 			"K.......", ginfo, ginfo, color::white, 0ull
 		);
-		auto res = g.perft(1);
-		REQUIRE(res.captures == 1);
-		REQUIRE(res.nodes == 5);
+		validate_position(g, 1, perft_results{ 5, 1, 0, 0, 0 });
 	}
 
 	SECTION("Black to move - everything")
 	{
+		// TODO: transform position into FEN string
 		game g(
 			". . . . . . . ."
 			". . . p . p p ."
@@ -181,13 +192,12 @@ TEST_CASE("Move Generation pawn")
 			". . . . . . . ."
 			"K . . . . . . k", ginfo, ginfo, color::black, 0ull
 		);
-		auto res = g.perft(1);
-		REQUIRE(res.captures == 3);
-		REQUIRE(res.nodes == 10);
+		validate_position(g, 1, perft_results{ 10, 3, 0, 0, 0 });
 	}
 
 	SECTION("Overflow when attacking")
 	{
+		// TODO: transform position into FEN string
 		game g(
 			". . . . . . . ."
 			". . . . . . . ."
@@ -198,10 +208,7 @@ TEST_CASE("Move Generation pawn")
 			". . . . . . . ."
 			"K . . . . . . k", ginfo, ginfo, color::white, 0ull
 		);
-
-		auto res = g.perft(1);
-		REQUIRE(res.captures == 2);
-		REQUIRE(res.nodes == 7);
+		validate_position(g, 1, perft_results{ 7, 2, 0, 0, 0 });
 	}
 }
 
@@ -209,6 +216,7 @@ TEST_CASE("Move generation rook")
 {
 	SECTION("Rook 1")
 	{
+		// TODO: transform position into FEN string
 		game g(
 			"........"
 			"........"
@@ -219,16 +227,13 @@ TEST_CASE("Move generation rook")
 			"........"
 			"....K...", ginfo, ginfo, color::white, 0ULL
 		);
-
-		auto res = g.perft(1);
-		REQUIRE(res.captures == 1);
-		REQUIRE(res.nodes == 16);
-	
+		validate_position(g, 1, perft_results{ 16, 1, 0, 0, 0 });
 	}
 
 	SECTION("Rook 2")
 	{
-		game g2(
+		// TODO: transform position into FEN string
+		game g(
 			"R.....K."
 			"........"
 			"........"
@@ -238,15 +243,13 @@ TEST_CASE("Move generation rook")
 			"........"
 			"........", ginfo, ginfo, color::white, 0ULL
 		);
-
-		auto res = g2.perft(1);
-		REQUIRE(res.captures == 0);
-		REQUIRE(res.nodes == 17);
+		validate_position(g, 1, perft_results{ 17, 0, 0, 0, 0 });
 	}
 
 	SECTION("capture in every direction")
 	{
-		game g3(
+		// TODO: transform position into FEN string
+		game g(
 			"....k..K"
 			"........"
 			"........"
@@ -256,10 +259,7 @@ TEST_CASE("Move generation rook")
 			"...p...."
 			"........", ginfo, ginfo, color::white, 0ULL
 		);
-
-		auto res = g3.perft(1);
-		REQUIRE(res.captures == 4);
-		REQUIRE(res.nodes == 11);
+		validate_position(g, 1, perft_results{ 11, 4, 0, 0, 0 });
 	}
 }
 
@@ -267,6 +267,7 @@ TEST_CASE("Move Generation Bishop")
 {
 	SECTION("Bishop easy")
 	{
+		// TODO: transform position into FEN string
 		game g(
 			". . . . . . . ."
 			". . . k . . . ."
@@ -277,14 +278,13 @@ TEST_CASE("Move Generation Bishop")
 			". . . . . . . ."
 			"K . . . . . . .", ginfo, ginfo, color::white, 0ULL
 		);
-		auto res = g.perft(1);
-		REQUIRE(res.captures == 2);
-		REQUIRE(res.nodes == 11);
+		validate_position(g, 1, perft_results{ 11, 2, 0, 0, 0 });
 	}
 
 	SECTION("Bishop on msb")
 	{
-		game g2(
+		// TODO: transform position into FEN string
+		game g(
 			"B . . . . . . ."
 			". . . k . . . ."
 			". . . . . . . ."
@@ -294,14 +294,13 @@ TEST_CASE("Move Generation Bishop")
 			". . . . . . . ."
 			"K . . . . . . .", ginfo, ginfo, color::white, 0ULL
 		);
-		auto res = g2.perft(1);
-		REQUIRE(res.captures == 1);
-		REQUIRE(res.nodes == 6);
+		validate_position(g, 1, perft_results{ 6, 1, 0, 0, 0 });
 	}
 
 	SECTION("Bishop on lsb")
 	{
-		game g2(
+		// TODO: transform position into FEN string
+		game g(
 			". . . . . . . k"
 			". . . . . . . ."
 			". . . . . . . ."
@@ -311,13 +310,12 @@ TEST_CASE("Move Generation Bishop")
 			". . . . . . . ."
 			"K . . . . . . b", ginfo, ginfo, color::black, 0ULL
 		);
-		auto res = g2.perft(1);
-		REQUIRE(res.captures == 1);
-		REQUIRE(res.nodes == 7);
+		validate_position(g, 1, perft_results{ 7, 1, 0, 0, 0 });
 	}
 
 	SECTION("Capture in every direction")
 	{
+		// TODO: transform position into FEN string
 		game g(
 			". . . . . k . ."
 			". p . . . P . ."
@@ -328,17 +326,15 @@ TEST_CASE("Move Generation Bishop")
 			". . . . . . . ."
 			"K . . . . . . .", ginfo, ginfo, color::black, 0ULL
 		);
-		auto res = g.perft(1);
-		REQUIRE(res.captures == 4);
-		REQUIRE(res.nodes == 12);
+		validate_position(g, 1, perft_results{ 12, 4, 0, 0, 0 });
 	}
-
 }
 
 TEST_CASE("Check and Checkmates")
 {
 	SECTION("Backrank mate")
 	{
+		// TODO: transform position into FEN string
 		game g(
 			". . . Q . . . k"
 			". . . . . . p p"
@@ -349,16 +345,13 @@ TEST_CASE("Check and Checkmates")
 			". . . . . . . ."
 			". . . . K . . .", ginfo, ginfo, color::black, 0ull
 		);
-		auto res = g.perft(1);
-		REQUIRE(res.captures == 0);
-		REQUIRE(res.nodes == 0);
-		res = g.perft(3);
-		REQUIRE(res.captures == 0);
-		REQUIRE(res.nodes == 0);
+		validate_position(g, 1, perft_results{});
+		validate_position(g, 3, perft_results{});
 	}
 
 	SECTION("Check")
 	{
+		// TODO: transform position into FEN string
 		game g(
 			". . . . . . . ."
 			". . . b . b . ."
@@ -369,14 +362,12 @@ TEST_CASE("Check and Checkmates")
 			". . . . . . . ."
 			". . . . . . . .", ginfo, ginfo, color::black, 0ull
 		);
-
-		auto res = g.perft(1);
-		REQUIRE(res.captures == 0);
-		REQUIRE(res.nodes == 1);
+		validate_position(g, 1, perft_results{ 1, 0, 0, 0, 0 });
 	}
 
 	SECTION("Stalemate")
 	{
+		// TODO: transform position into FEN string
 		game g(
 			"k . . . . . . ."
 			". . . . . . . ."
@@ -387,18 +378,15 @@ TEST_CASE("Check and Checkmates")
 			". . . . . . . ."
 			". . . . . . . .", ginfo, ginfo, color::black, 0ull
 		);
-
-		auto res = g.perft(1);
-		REQUIRE(res.captures == 0);
-		REQUIRE(res.nodes == 0);
+		validate_position(g, 1, perft_results{ 1, 0, 0, 0, 0 });
 	}
-
 }
 
 TEST_CASE("Move Generation Queen")
 {
 	SECTION("Queen easy")
 	{
+		// TODO: transform position into FEN string
 		game g(
 			". . k . . . . ."
 			". . . . . . . ."
@@ -409,14 +397,13 @@ TEST_CASE("Move Generation Queen")
 			". . p . p . . ."
 			". . . . . . . .", ginfo, ginfo, color::white, 0ULL
 		);
-		auto res = g.perft(1);
-		REQUIRE(res.captures == 3);
-		REQUIRE(res.nodes == 24);
+		validate_position(g, 1, perft_results{ 24, 3, 0, 0, 0 });
 	}
 }
 
 TEST_CASE("Move Generation Knight")
 {
+	// TODO: transform position into FEN string
 	SECTION("Knight 1")
 	{
 		game g(
@@ -429,14 +416,13 @@ TEST_CASE("Move Generation Knight")
 			". . p . . . . ."
 			". . . . . . . .", ginfo, ginfo, color::white, 0ULL
 		);
-		auto res = g.perft(1);
-		REQUIRE(res.captures == 2);
-		REQUIRE(res.nodes == 13);
+		validate_position(g, 1, perft_results{ 13, 2, 0, 0, 0 });
 	}
 
 	SECTION("Knight 2")
 	{
-		game g2(
+		// TODO: transform position into FEN string
+		game g(
 			"N . . . K . . N"
 			". . . . . . . ."
 			". . . . . . . ."
@@ -446,102 +432,50 @@ TEST_CASE("Move Generation Knight")
 			". . . . . . . ."
 			"N . . . k . . N", ginfo, ginfo, color::white, 0ULL
 		);
-		auto res = g2.perft(1);
-		REQUIRE(res.captures == 2);
-		REQUIRE(res.nodes == 13);
+		validate_position(g, 1, perft_results{ 13, 2, 0, 0, 0 });
 	}
 }
 
-
 TEST_CASE("Move Generation Initial Perft", "[move_gen]")
 {
+	// initial game position
 	game g;
 
-
-	// Check initial position
 	SECTION("depth = 0") {
-		auto res = g.perft(0);
-		REQUIRE(res.nodes == 1);
+		validate_position(g, 0, perft_results{ 1, 0, 0, 0, 0 });
 	}
 
 	SECTION("depth = 1") {
-		auto res = g.perft(1);
-		REQUIRE(res.nodes == 20);
-		REQUIRE(res.castles == 0);
-		REQUIRE(res.en_passants == 0);
-		REQUIRE(res.promos == 0);
-		REQUIRE(res.captures == 0);
+		validate_position(g, 1, perft_results{ 20, 0, 0, 0, 0 });
 	}
 
 	SECTION("depth = 2") {
-		auto res = g.perft(2);
-		REQUIRE(res.nodes == 400);
-		REQUIRE(res.captures == 0);
-		REQUIRE(res.en_passants == 0);
-		REQUIRE(res.castles == 0);
-		REQUIRE(res.promos == 0);
+		validate_position(g, 2, perft_results{ 400, 0, 0, 0, 0 });
 	}
 
 	SECTION("depth = 3") {
-		auto res = g.perft(3);
-		REQUIRE(res.nodes == 8902);
-		REQUIRE(res.captures == 34);
+		validate_position(g, 3, perft_results{ 8902, 34, 0, 0, 0 });
 	}
-	
+
 	SECTION("depth = 4") {
-		auto res = g.perft(4);
-		REQUIRE(res.nodes == 197281);
-		REQUIRE(res.captures == 1576);
+		validate_position(g, 4, perft_results{ 197281, 1576, 0, 0, 0 });
 	}
 
 	SECTION("depth = 5")
 	{
-		auto res = g.perft(5);
-		REQUIRE(res.en_passants == 258);
-		REQUIRE(res.captures == 82719);
-		REQUIRE(res.nodes == 4865609);
+		validate_position(g, 5, perft_results{ 4865609, 82719, 258, 0, 0 });
 	}
 
-	SECTION("depth = 6") {
-		auto res = g.perft(6);
-		REQUIRE(res.nodes == 119060324ULL);
-		REQUIRE(res.captures == 2812008ULL);
-		REQUIRE(res.en_passants == 5248ULL);
-	}
-
-}
-
-namespace
-{
-	void benchmark_perft(int depth)
-	{
-		game g;
-		printf("========== BENCHMARK ==========\n");
-		printf("Initial Perft depth = %i\n",depth);
-		auto now = std::chrono::high_resolution_clock::now();
-		auto res = g.perft(depth);
-		auto then = std::chrono::high_resolution_clock::now();
-		printf("Print so that it doesn't optimize the result away: %i\n", (int)res.nodes);
-		printf("measured time in seconds: %f\n", std::chrono::duration_cast<std::chrono::milliseconds>(then - now).count() / 1000.f);
-		printf("===============================\n\n");
-	}
+    // TODO: this test takes 10 seconds, think of adding it again if test time doesn't matter too much
+	/*SECTION("depth = 6") {
+		validate_position(g, 6, perft_results{ 119060324ULL, 2812008ULL, 5248ULL, 0, 0 });
+	}*/
 }
 
 TEST_CASE("Benchmark")
 {
 	benchmark_perft(5);
+
+	// TODO: this test takes 10 seconds, think of adding it again if test time doesn't matter too much
 	//benchmark_perft(6);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
